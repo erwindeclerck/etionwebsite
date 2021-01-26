1de993ed7b7dd20f7c441c9c33ab596eb802d498
erwindeclerck <erwin.de.clerck@gmail.com>
2021-01-26 05:08:30 +0100

    First commit

---
+++ web/themes/custom/etion_bootstrap_sass/js/custom.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.etion_bootstrap_sass = {
+    attach: function(context, settings) {
+
+      // Custom code here
+
+    }
+  };
+
+})(jQuery, Drupal);
